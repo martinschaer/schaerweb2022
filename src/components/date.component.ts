792300a8a1c4874d279b142037a536e08ef041c6
Martin Schaer <martin@schaerweb.com>
2022-05-29 19:49:28 -0600

    Add date component

---
+++ src/components/date.component.ts
@@ -0,0 +1,47 @@
+import { t } from '../i18n'
+
+const template = document.createElement('template')
+template.innerHTML = `<span></span>`
+
+const formatStr = (str: string, ...args: (string | number)[]) =>
+  str.replace(/{(\d+)}/g, (match, number) =>
+    typeof args[number] !== 'undefined' ? args[number].toString() : match
+  )
+
+class DateComponent extends HTMLElement {
+  constructor() {
+    super()
+    const tC = template.content
+    this.attachShadow({ mode: 'open' }).appendChild(tC.cloneNode(true))
+  }
+
+  connectedCallback() {
+    const date = new Date(+this.getAttribute('unix') * 1000)
+    const diff = new Date().getTime() - date.getTime()
+    const seconds = Math.floor(diff / 1000)
+    const minutes = Math.floor(diff / 60000)
+    const hours = Math.floor(diff / 3600000)
+    const days = Math.floor(diff / (3600000 * 24))
+    const weeks = Math.floor(diff / (3600000 * 24 * 7))
+    // eslint-disable-next-line no-console
+    // console.table({ weeks, days, hours, minutes, seconds })
+    let formatted: string
+    if (seconds < 60) {
+      formatted = formatStr(t('{0} seconds ago'), seconds)
+    } else if (minutes < 60) {
+      formatted = formatStr(t('{0} minutes ago'), minutes)
+    } else if (hours < 24) {
+      formatted = formatStr(t('{0} hours ago'), hours)
+    } else if (days < 7) {
+      formatted = formatStr(t('{0} days ago'), days)
+    } else if (weeks < 5) {
+      formatted = formatStr(t('{0} weeks ago'), weeks)
+    } else {
+      formatted = date.toLocaleString()
+    }
+    this.shadowRoot.innerHTML = formatted
+  }
+
+  // disconnectedCallback() {}
+}
+customElements.define('schaerweb-date', DateComponent)
