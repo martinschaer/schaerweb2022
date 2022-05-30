939843ac9d2a348880954e43dbb40e05d43d7363
Martin Schaer <martin@schaerweb.com>
2022-05-29 19:49:11 -0600

    Convert main into ts

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import "normalize.css";
-import './styles/main.scss'
-
-// eslint-disable-next-line no-console
-console.log('Hello main.js')
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import 'normalize.css'
+import './styles/main.scss'
+import './components/date.component'
+
+// eslint-disable-next-line no-console
+console.log('Hello main.ts')
