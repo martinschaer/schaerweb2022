b1aebc4aff8ca9abdf700232371c8234756b09ec
Martin Corrales Schaer <m.corrales.schaer@accenture.com>
2022-09-20 09:51:18 -0600

    Race charts

--- assets/f1.ts
+++ assets/f1.ts
@@ -1,113 +1,12 @@
 import 'aframe'
-
-export {}
-
-type APIParams = {
-  year: number
-  round?: number
-}
-
-type Driver = {
-  driverId: string
-  permanentNumber: string
-  code: string
-  url: string
-  givenName: string
-  familyName: string
-  dateOfBirth: string
-  nationality: string
-}
-
-type Constructors = {
-  constructorId: string
-  url: string
-  name: string
-  nationality: string
-}
-
-type DriverStanding = {
-  points: string
-  position: string
-  positionText: string
-  wins: string
-  Driver: Driver
-  Constructors: Constructors
-}
-
-type Round = {
-  round: string
-  season: string
-  DriverStandings: DriverStanding[]
-}
-
-const endpoint = 'https://ergast.com/api/f1'
-
-const COLORS_BY_DRIVER_CODE: Record<string, string> = {
-  VER: 'blue',
-  PER: 'blue',
-  LEC: 'red',
-  RUS: 'turquoise',
-  SAI: 'red',
-  HAM: 'turquoise',
-  NOR: 'orange',
-  BOT: 'darkred',
-  OCO: 'dodgerblue',
-  GAS: 'white',
-  ALO: 'dodgerblue',
-  MAG: 'gray',
-  RIC: 'orange',
-  VET: 'darkgreen',
-  TSU: 'white',
-  ALB: 'skyblue',
-  STR: 'darkgreen',
-  ZHO: 'darkred',
-  MSC: 'gray',
-  HUL: 'darkgreen',
-  LAT: 'skyblue',
-}
-
-const driverPosInRound = (driverCode: string, round: Round) => {
-  for (let i = 0; i < round.DriverStandings.length; i += 1) {
-    if (round.DriverStandings[i].Driver.code === driverCode) {
-      return +round.DriverStandings[i].position
-    }
-  }
-  return round.DriverStandings.length + 1
-}
-
-const load = async (api: string, { year, round }: APIParams) => {
-  const res = await fetch(
-    `${endpoint}/${year}${round !== undefined ? `/${round}` : ''}/${api}.json`
-  )
-  if (!res.ok) {
-    throw new Error(`HTTP error! Status: ${res.status}`)
-  }
-  return res.json()
-}
-
-const loadDriverStandings = async (params: APIParams) => {
-  const apiResult = await load('driverStandings', params)
-  return apiResult.MRData.StandingsTable.StandingsLists
-}
-
-const loadAllDriverStandings = async (params: APIParams): Promise<Round[]> => {
-  const temp = await loadDriverStandings(params)
-  if (temp.length) {
-    return [
-      temp[0],
-      ...(await loadAllDriverStandings({
-        year: params.year,
-        round: params.round !== undefined ? params.round + 1 : 0,
-      })),
-    ]
-  }
-  return temp as Round[]
-}
-
-const loadAllDrivers = async (params: APIParams) => {
-  const apiResult = await load('drivers', params)
-  return apiResult.MRData.DriverTable.Drivers as Driver[]
-}
+import {
+  COLORS_BY_DRIVER_CODE,
+  Driver,
+  Round,
+  driverPosInRound,
+  loadAllDriverStandings,
+  loadAllDrivers,
+} from './services/ergast'
 
 const WALL_H = 4
 const CHART_H = 2.5
@@ -134,11 +33,12 @@ const renderAFrame = (driverCodes: string[], rounds: Round[]) => {
   driverCodes.forEach((driverCode) => {
     let lastX = -2
     let lastY = WALL_H
+    const lastRound = rounds[rounds.length - 1]
+    if (!lastRound) return
     const textScale = '0.4 0.4 0'
     const textFont = 'sourcecodepro'
     const textPosition = `${CHART_W / 2 - 0.15} ${driverPosToY(
-      driverPosInRound(driverCode, rounds[rounds.length - 1]) /
-        driverCodes.length
+      driverPosInRound(driverCode, lastRound) / driverCodes.length
     )} -${DISTANCE_TO_CENTER}`
     scene += `<a-text value="${driverCode}" position="${textPosition}" font="${textFont}" scale="${textScale}"></a-text>`
     rounds.forEach((round, roundIndex) => {
